82b991240a5aa3d05fa07138e55fd4479caa64ae
guimaisan <guimaisan@outlook.com>
2023-10-08 14:34:16 -0700

    made up arrow input for capsule
    make sure to use the sample scene
    (look in the scenes folder)

--- Sum Game wit Bern/Assets/moving_is_fun.cs
+++ Sum Game wit Bern/Assets/moving_is_fun.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class moving_is_fun : MonoBehaviour
 {
-
+    float movSpeed = 2;
     public Transform pos;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        
+    
     }
+    
+
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.position += Vector3.up * Time.deltaTime * movSpeed;
+        }
     }
 }
