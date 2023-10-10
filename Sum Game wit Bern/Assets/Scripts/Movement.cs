6745e366b1354908836e564319968b3378545606
guimaisan <guimaisan@outlook.com>
2023-10-09 17:08:43 -0700

    Made sprite cuz yes and re-coded the movement
    hopefully we can make tile maps for the map and also walls + colliders

--- Sum Game wit Bern/Assets/Scripts/Movement.cs
+++ Sum Game wit Bern/Assets/Scripts/Movement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float movSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movSpeed = 2;
     }
-
     // Update is called once per frame
     void Update()
-    {
-        
+    {   //making WASD movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * Time.deltaTime * movSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * movSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * movSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * movSpeed;
+        }
     }
 }
